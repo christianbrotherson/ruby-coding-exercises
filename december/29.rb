95c40bf02f1df0261bf34987cc0276f3235a1087
christianbrotherson <christian.brotherson@gmail.com>
2017-02-28 14:07:41 -0700

    worked on 29.rb exercise

--- december/29.rb
+++ december/29.rb
@@ -1,5 +1,21 @@
 require 'rspec'
 
+def bubble_sort(arr)
+	return arr if arr.size <= 1
+
+	loop do
+		swapped = false
+		0.upto( arr.size - 2 ) do |i|
+			if list[i] > list[i + 1]
+				list[i], list[i + 1] = list[i + 1], list[i]
+				swapped = true
+			end
+		end
+		break unless swapped
+	end
+end
+
+
 describe 'Adding bubble sort to the Array class' do
   it 'Properly sorts an array' do
     arr = [4, 1, 6, 10, 44, 2, 3]
