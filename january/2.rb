a53a813a0943bb10868e2dc994d3c8a79aa80947
christianbrotherson <christian.brotherson@gmail.com>
2017-03-03 13:30:11 -0700

    completed jan 2.rb exercise

--- january/2.rb
+++ january/2.rb
@@ -1,19 +1,22 @@
 require 'rspec'
 
-# <!doctype html>
 
-# <html lang="en">
-# <head>
-#   <meta charset="utf-8">
-# 
-#   <title>Title Goes Here</title>
-# </head>
-# 
-# <body>
-# </body>
-# </html>
 
 def html_generator title
+<<HTML
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>#{ title }</title>
+</head>
+
+<body>
+</body>
+</html>
+HTML
 end
 
 describe 'HTML generator' do
@@ -21,3 +24,6 @@ describe 'HTML generator' do
     expect(html_generator 'My Site').to match(/My Site/)
   end
 end
+
+
+html_generator "My Title"
